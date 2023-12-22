ecd1e1fa088f551b970ecd1096bf4d3ea34310e3
John Chen <john.hk.chen@gmail.com>
2023-12-22 11:07:35 -0800

    get 'full' unit test coverage

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
-#[allow(unused_imports)]
 use std::env;
-#[allow(unused_imports)]
-use std::fs;
 
+use git_starter_rust::run;
+
+#[cfg(not(tarpaulin_include))]
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
-    }
+    let args: Vec<String> = env::args().skip(1).collect();
+    let message = run(&args);
+    print!("{message}");
 }
